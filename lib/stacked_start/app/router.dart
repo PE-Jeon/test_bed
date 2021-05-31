41ab94ec7bd8d92e839b1394bc42032b6e14fd83
junhyun <pe.jeon@gmail.com>
2021-05-31 23:32:17 +0900

    Auto Route generation error fixed

--- lib/stacked_start/app/router.dart
+++ lib/stacked_start/app/router.dart
@@ -1,15 +1,16 @@
 
-
-
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:test_bed/stacked_start/ui/views/home/home_view.dart';
 import 'package:test_bed/stacked_start/ui/views/home/startup_view.dart';
 
-@MaterialAutoRouter()
+@MaterialAutoRouter(
+  routes: <AutoRoute>[
+    MaterialRoute(page: StartupView, initial: true),
+    MaterialRoute(page: HomeView),
+  ]
+)
+
+
 class $Router {
 
-  @initial
-  StartupView startupViewRoute;
-
-  HomeView homeViewRoute;
 }
